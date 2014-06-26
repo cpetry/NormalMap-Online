681c8012548a512f437b9de9792a1c6c8b8922a6
Christian Petry <petry2christian@gmail.com>
2014-06-26 14:31:04 +0200

    init upload

---
+++ javascripts/ImageDrop.js
@@ -0,0 +1,118 @@
+var height_image;
+var max_height = 300;
+
+
+var initHeightMap = function(){
+	var div_container = document.getElementById("height_map");
+	div_container.setAttribute("style","width:" + max_height + "px; height:" + max_height + "px;");
+	
+	var canvas = document.getElementById('height_canvas');
+	canvas.setAttribute("style","width:" + max_height + "px; height:" + max_height + "px;");
+	var context = canvas.getContext('2d');
+    height_image = new Image();
+
+	height_image.onload = function () {
+		context.drawImage(height_image, 0, 0, height_image.width, height_image.height, 0,0, canvas.width, canvas.height);
+		height_image.width = height_image.naturalWidth;
+		height_image.height = height_image.naturalHeight;
+    };
+	
+	
+	
+    height_image.src = './images/default_pic.png';
+}
+
+
+initHeightMap();
+
+
+
+
+require(["dojo/dom", "dojo/domReady!"], function(dom){
+	var height_map_drop = dom.byId("height_map"),
+		height_canvas = dom.byId("height_canvas");
+		
+	
+
+	//	DOMReady setup
+	height_map_drop.addEventListener("dragover", function(e) {e.preventDefault();}, true);
+	height_map_drop.addEventListener("drop", function(e){
+		e.preventDefault(); 
+		readImage(e.dataTransfer.files[0]);
+	}, true);
+	
+	
+	
+	var readImage = function(imgFile){
+		if(!imgFile.type.match(/image.*/)){
+			console.log("The dropped file is not an image: ", imgFile.type);
+			return;
+		}
+
+		var reader = new FileReader();
+		reader.onload = function(e){
+			render(e.target.result);
+		};
+		reader.readAsDataURL(imgFile);
+	};
+
+	
+	
+	var render = function(source){
+		height_image = new Image();
+		height_image.onload = function(){
+			if(height_image.height > max_height) {
+				height_image.width *= max_height / height_image.height;
+				height_image.height = max_height;
+			}
+			else{
+				height_image.width = max_height;
+				height_image.height = max_height;
+			}
+			
+			var context = height_canvas.getContext("2d");
+			context.clearRect(0, 0, height_canvas.width, height_canvas.height);
+			height_map_drop.style.width = height_image.width + "px";
+			height_map_drop.style.height = height_image.height + "px";
+			height_canvas.width = height_image.width;
+			height_canvas.height = height_image.height;
+			
+			context.drawImage(height_image, 0, 0, height_image.width, height_image.height);
+		};
+		
+		height_image.src = source;
+	};
+	
+	
+});
+
+
+
+
+
+
+var createNormalMap = function(){
+	var normal_canvas = document.getElementById("normal_canvas");
+	
+	var ctx_normal = normal_canvas.getContext("2d");
+	ctx_normal.clearRect(0, 0, normal_canvas.width, normal_canvas.height);
+	normal_canvas.width = height_image.width;
+	normal_canvas.height = height_image.height;
+	
+	var grayscale = Filters.filterImage(Filters.grayscale, height_image);
+	
+	// Note that ImageData values are clamped between 0 and 255, so we need
+	// to use a Float32Array for the gradient values because they
+	// range between -255 and 255.
+	
+	var img_data = Filters.newsobelfilter(grayscale, document.getElementById("strength_slider").value, document.getElementById("level_slider").value);
+	var idata = Filters.createImageData(img_data.width, img_data.height);
+	
+	for (var i=0; i<img_data.data.length; i++)
+		idata.data[i] = img_data.data[i];
+	
+	ctx_normal.putImageData(idata, 0, 0);
+}
+
+
+
---
+++ javascripts/filters.js
@@ -0,0 +1,144 @@
+
+function Filters()
+{
+}
+
+Filters.getPixels = function(img) {
+	
+	var c = this.getCanvas(img.width, img.height);
+	var ctx = c.getContext('2d');
+	ctx.drawImage(img, 0, 0,img.width, img.height);
+	return ctx.getImageData(0, 0, c.width, c.height);
+};
+
+Filters.getCanvas = function(w,h) {
+  var c = document.createElement('canvas');
+  c.width = w;
+  c.height = h;
+  return c;
+};
+
+Filters.filterImage = function(filter, image, var_args) {
+  var args = [this.getPixels(image)];
+  for (var i=2; i<arguments.length; i++) {
+	args.push(arguments[i]);
+  }
+  return filter.apply(null, args);
+};
+
+Filters.brightness = function(pixels, adjustment) {
+  var d = pixels.data;
+  for (var i=0; i<d.length; i+=4) {
+	d[i] += adjustment;
+	d[i+1] += adjustment;
+	d[i+2] += adjustment;
+  }
+  return pixels;
+};
+
+Filters.tmpCanvas = document.createElement('canvas');
+Filters.tmpCtx = Filters.tmpCanvas.getContext('2d');
+
+Filters.createImageData = function(w,h) {
+  return this.tmpCtx.createImageData(w,h);
+};
+
+
+Filters.convoluteFloat32 = function(pixels, weights, opaque) {
+  var side = Math.round(Math.sqrt(weights.length));
+  var halfSide = Math.floor(side/2);
+
+  var src = pixels.data;
+  var sw = pixels.width;
+  var sh = pixels.height;
+
+  var w = sw;
+  var h = sh;
+  var output = {
+	width: w, height: h, data: new Float32Array(w*h*4)
+  };
+  var dst = output.data;
+
+  var alphaFac = opaque ? 1 : 0;
+
+  for (var y=0; y<h; y++) {
+	for (var x=0; x<w; x++) {
+	  var sy = y;
+	  var sx = x;
+	  var dstOff = (y*w+x)*4;
+	  var r=0, g=0, b=0, a=0;
+	  for (var cy=0; cy<side; cy++) {
+		for (var cx=0; cx<side; cx++) {
+		  var scy = Math.min(sh-1, Math.max(0, sy + cy - halfSide));
+		  var scx = Math.min(sw-1, Math.max(0, sx + cx - halfSide));
+		  var srcOff = (scy*sw+scx)*4;
+		  var wt = weights[cy*side+cx];
+		  r += src[srcOff] * wt;
+		  g += src[srcOff+1] * wt;
+		  b += src[srcOff+2] * wt;
+		  a += src[srcOff+3] * wt;
+		}
+	  }
+	  dst[dstOff] = r;
+	  dst[dstOff+1] = g;
+	  dst[dstOff+2] = b;
+	  dst[dstOff+3] = a + alphaFac*(255-a);
+	}
+  }
+  return output;
+};
+
+
+Filters.grayscale = function(pixels, args) {
+  var d = pixels.data;
+  for (var i=0; i<d.length; i+=4) {
+	var r = d[i];
+	var g = d[i+1];
+	var b = d[i+2];
+	// CIE luminance for the RGB
+	// The human eye is bad at seeing red and blue, so we de-emphasize them.
+	var v = 0.2126*r + 0.7152*g + 0.0722*b;
+	d[i] = d[i+1] = d[i+2] = v
+  }
+  return pixels;
+};
+
+Filters.newsobelfilter = function(pixels, strength, level){
+	var src = pixels.data;
+
+	var w = pixels.width;
+	var h = pixels.height;
+	var output = {
+		width: w, height: h, data: new Float32Array(w*h*4)
+	};
+	
+	var dst = output.data;
+	
+	for (var y=0; y<h; y++) {
+		for (var x=0; x<w; x++) {
+			var dstOff = (y*w+x)*4;
+	
+			var tl = src[Math.min(Math.max(dstOff - 4 - w*4,0),w*h*4-1)];   // top left  
+			var  l = src[Math.min(Math.max(dstOff - 4      ,0),w*h*4-1)];   // left  
+			var bl = src[Math.min(Math.max(dstOff - 4 + w*4,0),w*h*4-1)];   // bottom left  
+			var  t = src[Math.min(Math.max(dstOff - w*4    ,0),w*h*4-1)];   // top  
+			var  b = src[Math.min(Math.max(dstOff + w*4    ,0),w*h*4-1)];   // bottom  
+			var tr = src[Math.min(Math.max(dstOff + 4 - w*4,0),w*h*4-1)];   // top right  
+			var  r = src[Math.min(Math.max(dstOff + 4      ,0),w*h*4-1)];   // right  
+			var br = src[Math.min(Math.max(dstOff + 4 + w*4,0),w*h*4-1)];   // bottom right  
+    
+   
+			var dX = tr + 2.0*r + br -tl - 2.0*l - bl;
+			var dY = bl + 2.0*b + br -tl - 2.0*t - tr;
+			var dZ = 1.0 / strength * (1.0 + Math.pow(2.0, level));
+			
+			var l = Math.sqrt((dX * dX) + (dY * dY) + (dZ * dZ));
+			
+			dst[dstOff] = (dX/l * 0.5 + 0.5) * 255; 	// red
+			dst[dstOff+1] = (dY/l * 0.5 + 0.5) * 255; 	// green
+			dst[dstOff+2] = dZ/l * 255; 				// blue
+			dst[dstOff+3] = 255;
+		}
+	}
+	return output;
+}
