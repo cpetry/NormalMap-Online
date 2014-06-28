8a9c47082ece0f54c46be766b8fded71bd986e37
Christian Petry <petry2christian@gmail.com>
2014-06-28 14:53:29 +0200

    added some viewing stuff

--- javascripts/ImageDrop.js
+++ javascripts/ImageDrop.js
@@ -30,9 +30,11 @@ var initHeightMap = function(){
 var scene;
 var camera;
 var renderer;
-var cube;
+var model;
 var texture;
 var normal_map;
+var material;
+var rotation_enabled = 0;
 
 var initRenderer = function(){
 
@@ -43,11 +45,12 @@ var initRenderer = function(){
 	renderer.setSize( container_height, container_height );
 	document.getElementById('render_view').appendChild( renderer.domElement );
 
-	var geometry = new THREE.BoxGeometry(1,1,1);
 	var height_canvas   = document.getElementById('height_canvas');
 	texture  		= new THREE.Texture( height_canvas );
 	normal_map  	= new THREE.Texture( normal_canvas );
-	var material 	= new THREE.MeshPhongMaterial ( { 
+	normal_map.wrapS = normal_map.wrapT = THREE.RepeatWrapping;
+	
+	material = new THREE.MeshPhongMaterial ( { 
 		ambient: 0xbbbbbb, 
         color: 0xbbbbbb,
 		specular: 0x555555,
@@ -58,8 +61,9 @@ var initRenderer = function(){
         skining: true
 	} );
 	
-	cube = new THREE.Mesh( geometry, material);
-	scene.add( cube );
+	var geometry = new THREE.BoxGeometry(1,1,1);
+	model = new THREE.Mesh( geometry, material);
+	scene.add( model );
 	
 	// add subtle ambient lighting
 	var ambientLight = new THREE.AmbientLight(0xaaaaaa);
@@ -73,14 +77,57 @@ var initRenderer = function(){
 	
 	
 	camera.position.z = 1.5;
-
 }
+
+var setRepeat = function(v_x, v_y){
+	normal_map.repeat.set( v_x, v_y );
+}
+
+var setModel = function(type){
+	scene.remove( model );
+
+	if (type == "Cube"){
+		var geometry = new THREE.BoxGeometry(1,1,1);
+		model = new THREE.Mesh( geometry, material);
+		scene.add( model );
+	}
+	else if (type == "Sphere"){
+		var geometry = new THREE.SphereGeometry( 0.7, 32, 32);
+		model = new THREE.Mesh( geometry, material);
+		scene.add( model );
+	}
+	else if (type == "Cylinder"){
+		var geometry = new THREE.CylinderGeometry( 0.7, 0.5, 1, 32 );
+		model = new THREE.Mesh( geometry, material);
+		scene.add( model );
+	}
+	else if (type == "Plane"){
+		var geometry = new THREE.PlaneGeometry(2,2);
+		rotation_enabled = 0;
+		model.rotation.x = 0;
+		model.rotation.y = 0;
+		document.getElementById('input_rot').checked = false;
+		model = new THREE.Mesh( geometry, material);
+		scene.add( model );
+	}
+	
+}
+
+function toggleRotation(){
+	if(rotation_enabled)
+		rotation_enabled=0;
+	else
+		rotation_enabled=1;
+}
+
 
 function render() {
 	requestAnimationFrame(render);
 	renderer.render(scene, camera);
-	cube.rotation.x += 0.003;
-	cube.rotation.y += 0.003;
+	if(rotation_enabled){
+		model.rotation.x += 0.003;
+		model.rotation.y += 0.003;
+	}
 	normal_map.needsUpdate = true;
 	texture.needsUpdate = true;
 }
